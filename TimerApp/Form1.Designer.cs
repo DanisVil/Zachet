644690cd0e2dbb4931398caf987e550db26c77ad
Danis222 <danisvil@mail.ru>
2022-05-20 21:04:28 +0300

    Добавьте файлы проекта.

---
+++ TimerApp/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace TimerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.hourTextBox = new System.Windows.Forms.TextBox();
+            this.minutesTextBox = new System.Windows.Forms.TextBox();
+            this.secondsTextBox = new System.Windows.Forms.TextBox();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // hourTextBox
+            // 
+            this.hourTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.hourTextBox.Location = new System.Drawing.Point(78, 115);
+            this.hourTextBox.Name = "hourTextBox";
+            this.hourTextBox.Size = new System.Drawing.Size(100, 45);
+            this.hourTextBox.TabIndex = 0;
+            // 
+            // minutesTextBox
+            // 
+            this.minutesTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.minutesTextBox.Location = new System.Drawing.Point(305, 115);
+            this.minutesTextBox.Name = "minutesTextBox";
+            this.minutesTextBox.Size = new System.Drawing.Size(100, 45);
+            this.minutesTextBox.TabIndex = 1;
+            // 
+            // secondsTextBox
+            // 
+            this.secondsTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.secondsTextBox.Location = new System.Drawing.Point(529, 115);
+            this.secondsTextBox.Name = "secondsTextBox";
+            this.secondsTextBox.Size = new System.Drawing.Size(100, 45);
+            this.secondsTextBox.TabIndex = 2;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Enabled = false;
+            this.cancelButton.Location = new System.Drawing.Point(78, 314);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(100, 34);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Отмена";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(529, 314);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(100, 34);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Старт";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(318, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 382);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.secondsTextBox);
+            this.Controls.Add(this.minutesTextBox);
+            this.Controls.Add(this.hourTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Таймер";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox hourTextBox;
+        private System.Windows.Forms.TextBox minutesTextBox;
+        private System.Windows.Forms.TextBox secondsTextBox;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TimerApp/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerApp
+{
+    public partial class Form1 : Form
+    {
+        private int seconds, minutes, hours;
+        private const string numbers = "0123456789";
+        private bool isGoing = false, paused = false;
+        private int tick;
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Hide();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+        }
+        private void CalculateTime(int time)
+        {
+            hours = time / 3600;
+            time %= 3600;
+            minutes = time / 60;
+            time %= 60;
+            seconds = time;
+            label1.Text = $"{hours}:{minutes}:{seconds}";
+        }
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (isGoing)
+            {
+                if (!paused)
+                {
+                    startButton.Text = "Дальше";
+                    paused = true;
+                    timer1.Stop();
+                }
+                else
+                {
+                    startButton.Text = "Пауза";
+                    paused = false;
+                    timer1.Start();
+                }
+            }
+            else if (CheckTime())
+            {
+                secondsTextBox.Hide();
+                minutesTextBox.Hide();
+                hourTextBox.Hide();
+                int s = Convert.ToInt32(secondsTextBox.Text);
+                int m = Convert.ToInt32(minutesTextBox.Text);
+                int h = Convert.ToInt32(hourTextBox.Text);
+                int time = s + m * 60 + h * 3600;
+                CalculateTime(time);
+                tick = time;
+                timer1.Start();
+                isGoing = true;
+                label1.Show();
+                cancelButton.Enabled = true;
+                startButton.Text = "Пауза";
+            }
+        }
+        private bool CheckTime()
+        {
+            string a = new string(hourTextBox.Text.Intersect(numbers).ToArray());
+            if (new string(hourTextBox.Text.Intersect(numbers).ToArray()).Equals("")&&
+                new string(minutesTextBox.Text.Intersect(numbers).ToArray()).Equals("")&&
+                new string(secondsTextBox.Text.Intersect(numbers).ToArray()).Equals("") ||
+                hourTextBox.Text.Equals("") || secondsTextBox.Text.Equals("")|| minutesTextBox.Text.Equals(""))
+            {
+                MessageBox.Show("Неверный формат");
+                return false;
+            }
+            return true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (tick > 1)
+            {
+                tick--;
+            }
+            else if (isGoing)
+            {
+                timer1.Stop();
+                MessageBox.Show("Все!");
+            }
+            int time = tick;
+            CalculateTime(time);
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            isGoing = false;
+            cancelButton.Enabled = false;
+            startButton.Text = "Старт";
+            paused = false;
+            timer1.Stop();
+            label1.Hide();
+            secondsTextBox.Show();
+            minutesTextBox.Show();
+        }
+    }
+}
